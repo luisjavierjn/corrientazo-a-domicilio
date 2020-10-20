431d0bb8b8d4ef1a4ca82f2343e6cb4988f3237d
Luis Jiménez <luis.jimenez@smartmatic.com>
2020-10-19 21:13:00 -0500

    adding writing files capability

---
+++ outbound/src/main/java/com/corrientazo/outbound/FileOutput.java
@@ -0,0 +1,48 @@
+package com.corrientazo.outbound;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileOutput {
+
+    private String path;
+    private String header;
+    private List<String> lines;
+
+    public FileOutput(String path, String header) {
+        this.path = path;
+        this.header = header;
+        this.lines = new ArrayList<>();
+    }
+
+    public void addNewLine(String line) {
+        lines.add(line);
+    }
+
+    public void write(String fileName) throws IOException {
+        File fout = new File(path.concat("/").concat(fileName));
+        boolean fileExist = fout.exists();
+        try(FileOutputStream fos = new FileOutputStream(fout, fout.exists())) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            if(!fileExist) {
+                bw.write(header);
+                bw.newLine();
+            }
+            lines.forEach(l -> {
+                try {
+                    bw.write(l);
+                    bw.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            lines.clear();
+            bw.close();
+        }
+    }
+}
